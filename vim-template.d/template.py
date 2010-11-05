a0e2f9e4b335bbc0681491bee39f3cb73b09f899
Óscar García Amor <ogarcia@connectical.com>
2010-11-05 18:18:48 +0100

    Added vim configuration files

---
+++ vim-template.d/template.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+%FFILE%
+Copyright (C) %YEAR% %USER% <%MAIL%>
+
+Distributed under terms of the MIT license.
+"""
+
+%HERE%
