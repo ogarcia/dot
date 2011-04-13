249853835810f66bf2271600496d02c22190a0c0
Óscar García Amor <ogarcia@connectical.com>
2011-04-13 16:16:56 +0200

    Removed unnecessary IF sentence in git.bash file

--- bash_auto.d/app/git.bash
+++ bash_auto.d/app/git.bash
@@ -1,6 +1,7 @@
 # This file is sourced by .bashrc. This script provide a suite
 # of git(1) specified utils. This file was contributed by
 # Adrian Perez <aperez at connectical.com>
+# Oscar Garcia <ogarcia at connectical.com>
 
 if ! installed git; then
 	return 0
@@ -15,10 +16,8 @@ git_get_branch_name ()
 
 git_set_branch_in_prompt ()
 {
-	if [[ -x /usr/bin/git ]] ; then
 	[[ $prompt = *git_get_branch_name* ]] \
   	|| export prompt="\[\e[0;36m\]\$(git_get_branch_name)\[\e[0;0m\]$prompt"
-	fi
 }
 prompt_hook_git=git_set_branch_in_prompt
 
