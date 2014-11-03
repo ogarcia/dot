1dc6d13a368f7927303e2e0e87514fd210a5e13e
Óscar García Amor <ogarcia@connectical.com>
2014-11-03 19:56:50 +0100

    Better color and bash_completion scripts

--- bash_auto.d/cfg/bash_completion.bash
+++ bash_auto.d/cfg/bash_completion.bash
@@ -4,6 +4,12 @@
 [ -f /etc/bash_completion ] && \
 	. /etc/bash_completion
 
+if installed brew; then
+	in_brpr=$(brew --prefix)
+	[ -f ${in_brpr}/etc/bash_completion ] && \
+		. ${in_brpr}/etc/bash_completion
+fi
+
 if [ -d ~/.bash_completion.d/ ]; then
 	for mod in $(ls -c1 ~/.bash_completion.d/) ; do
 		. ~/.bash_completion.d/$mod
--- bash_auto.d/cfg/color.bash
+++ bash_auto.d/cfg/color.bash
@@ -3,23 +3,11 @@
 
 options_grep="${options_grep} --color=auto"
 
-[ "${OS}" = "Darwin" ] && case ${TERM} in
-	*color | xterm | linux )
+case ${TERM} in
+	xterm* | screen* | *color )
 		export CLICOLOR=1
 		alias grep="grep ${options_grep}"
-		return 0
 	;;
 esac
-
-if ! installed dircolors
-then return 0
-else
-	eval $(dircolors -b)
-fi
-
-options_ls="${options_ls} --color=auto"
-
-alias ls="ls ${options_ls}"
-alias grep="grep ${options_grep}"
 
 # -- end -- vim:ft=sh:
