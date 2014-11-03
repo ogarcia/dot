c37a8f6af83f0d05bad1b1321e4234aea4218498
Óscar García Amor <ogarcia@connectical.com>
2014-11-03 11:38:22 +0100

    Add full color support to some terminals

---
+++ bash_auto.d/cfg/xterm.bash
@@ -0,0 +1,11 @@
+# This file is sourced by .bashrc. This script configures
+# some X terminals to have full color support.
+
+if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+	case "$TERM" in
+		xterm*) export TERM='xterm-256color' ;;
+		screen*) export TERM='screen-256color' ;;
+	esac
+fi
+
+# -- end -- vim:ft=sh:
