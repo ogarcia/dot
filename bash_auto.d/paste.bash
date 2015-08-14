45b719d19094927117f61c47d06e3ba2f90dc2f0
Óscar García Amor <ogarcia@connectical.com>
2015-08-14 20:41:08 +0200

    New 'pastebin-like' commands

---
+++ bash_auto.d/paste.bash
@@ -0,0 +1,20 @@
+# This file is sourced by .bashrc. This script configures
+# several online paste commands of pastebin style.
+
+if ! installed curl || ! installed awk; then
+	return 0
+fi
+
+nnpaste () {
+	a=$(cat)
+	curl -X POST -s -d "$a" http://pastein.connectical.com/documents | \
+	  awk -F '"' '{print "http://pastein.connectical.com/"$4}'
+}
+
+haste () {
+	a=$(cat)
+	curl -X POST -s -d "$a" http://hastebin.com/documents | \
+	  awk -F '"' '{print "http://hastebin.com/"$4}'
+}
+
+# -- end -- vim:ft=sh:
