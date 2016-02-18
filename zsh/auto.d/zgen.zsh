3acbe167a26993b181f1dfe4d8be1eaec7e0997d
Óscar García Amor <ogarcia@connectical.com>
2016-02-18 13:17:52 +0100

    Added Zgen support

---
+++ zsh/auto.d/zgen.zsh
@@ -0,0 +1,14 @@
+# This file is sourced by .zshrc. This script loads zgen functionality where
+# available.
+
+if [[ -r ~/.zsh/zgen/zgen.zsh ]]; then
+  ZGEN_DIR=~/.zsh/zgen
+  source ~/.zsh/zgen/zgen.zsh
+  if ! zgen saved; then
+    zgen load zsh-users/zsh-completions src
+    zgen load aperezdc/virtualz
+    zgen save
+  fi
+fi
+
+# -- end -- vim:ft=zsh:
