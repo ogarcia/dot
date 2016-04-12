2eb31e96a072b655da6def62873777113c4a35d9
Óscar García Amor <ogarcia@connectical.com>
2016-04-12 10:28:46 +0200

    Locales must be defined in local_auto.d

--- bash/auto.d/locale.bash
+++
@@ -1,6 +0,0 @@
-# This file is sourced by .bashrc. This script contains
-# locale definitions.
-
-export LANG="es_ES.UTF-8"
-
-# -- end -- vim:ft=sh:
--- zsh/auto.d/locale.zsh
+++
@@ -1,5 +0,0 @@
-# This file is sourced by .zshrc. This script contains locale definitions.
-
-export LANG="es_ES.UTF-8"
-
-# -- end -- vim:ft=zsh:
