21c96304bdb845a0d6e5b55dd68f2bd0f3e316a4
Óscar García Amor <ogarcia@connectical.com>
2016-12-29 00:16:21 +0100

    Use my own zgen fork

--- zsh/auto.d/zgen.zsh
+++ zsh/auto.d/zgen.zsh
@@ -3,10 +3,12 @@
 
 if [[ -r ~/.zsh/zgen/zgen.zsh ]]; then
   ZGEN_DIR=~/.zsh/zgen
+  ZGEN_AUTOLOAD_COMPINIT=1
+  [[ ! -d ~/.zsh/zcompdump ]] && mkdir -p ~/.zsh/zcompdump
+  ZGEN_CUSTOM_COMPDUMP=~/.zsh/zcompdump/zgenzcompdump
   VIRTUALZ_HOME=~/.local/virtualenvs
   source ~/.zsh/zgen/zgen.zsh
   if ! zgen saved; then
-    zgen load zsh-users/zsh-syntax-highlighting
     zgen load zsh-users/zsh-completions src
     zgen load Tarrasch/zsh-autoenv
     zgen load aperezdc/virtualz
@@ -15,7 +17,7 @@ if [[ -r ~/.zsh/zgen/zgen.zsh ]]; then
 else
   zgen-install () {
     { [[ -d ~/.zsh ]] || mkdir ~/.zsh; } && \
-      git clone git://github.com/tarjoilija/zgen ~/.zsh/zgen && \
+      git clone --depth=1 git://github.com/ogarcia/zgen ~/.zsh/zgen && \
       exec "${SHELL}" -l
   }
 fi
