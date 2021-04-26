220de0dc7b754491b6ad5204c40cd04d35528908
Óscar García Amor <ogarcia@connectical.com>
2021-04-26 09:45:16 +0200

    More rust configuration

--- zsh/auto.d/rust.zsh
+++ zsh/auto.d/rust.zsh
@@ -10,4 +10,12 @@ fi
 export CARGO_HOME=~/.local/cargo
 export PATH=${PATH}:~/.local/cargo/bin
 
+if ! installed rustup
+  then return 0
+fi
+
+[ ! -d ~/.local/rustup ] && mkdir -p ~/.local/rustup
+
+export RUSTUP_HOME=~/.local/rustup
+
 # -- end -- vim:ft=zsh:
