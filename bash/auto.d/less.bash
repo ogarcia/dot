314d9f22b6c9bc444b6a32f2cd14af5c8323bc5e
Óscar García Amor <ogarcia@connectical.com>
2023-07-05 09:44:14 +0200

    Fixes less command options

--- bash/auto.d/less.bash
+++ bash/auto.d/less.bash
@@ -9,13 +9,6 @@ fi
 
 # Set a fancy status bar un less(1) program.
 in_mprompt="    ?ltLine %lt-%lb ?pt(%pt\%).:?btByte..     [?f%f:*stdin*.] ?m(%i-%m).   "
-export LESS="-J -K -M -x4 -PM[1;34;43m$in_mprompt[0;0m"
-
-# SuSE doesn't support "raw" control characters.
-{ [ -e "/etc/SuSE-release" ] && export LESS="-R $LESS"; } || export LESS="-r $LESS"
-
-# By default use less(1) in raw mode (allow color codes
-# and more).
-alias less='less -R'
+export LESS="-R -J -K -M -x4 -PM[1;34;43m$in_mprompt[0;0m"
 
 # -- end -- vim:ft=sh:
--- zsh/auto.d/less.zsh
+++ zsh/auto.d/less.zsh
@@ -7,10 +7,7 @@ fi
 
 # Set a fancy status bar un less(1) program.
 in_mprompt="    ?ltLine %lt-%lb ?pt(%pt\%).:?btByte..     [?f%f:*stdin*.] ?m(%i-%m).   "
-export LESS="-J -K -M -x4 -PM[48;5;233m[38;5;75m$in_mprompt[0;0m"
-
-# SuSE doesn't support "raw" control characters.
-{ [ -e "/etc/SuSE-release" ] && export LESS="-R $LESS"; } || export LESS="-r $LESS"
+export LESS="-R -J -K -M -x4 -PM[48;5;233m[38;5;75m$in_mprompt[0;0m"
 
 # TERMCAP codes
 # vb      flash     emit visual bell
@@ -28,9 +25,5 @@ export LESS_TERMCAP_so=$'\e[48;5;235m\e[38;5;242m'
 export LESS_TERMCAP_se=$'\e[0;0m'
 export LESS_TERMCAP_us=$'\e[38;5;71m'
 export LESS_TERMCAP_ue=$'\e[0;0m'
-
-# By default use less(1) in raw mode (allow color codes
-# and more).
-alias -- less='less -R'
 
 # -- end -- vim:ft=zsh:
